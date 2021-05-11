1d289ef1bcb5273b6533720ac4e4e08585bf3022
Sam Chun <sc.wjstoa@gmail.com>
2021-05-11 13:19:02 -0700

    normalized input data and loaded train/val sets

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import pickle
+import math
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import random_split
 
 
 class NotesDataset(Dataset):
@@ -33,6 +35,9 @@ def prepare_sequences(notes_path, sequence_length):
         network_input.append([note_to_int[note] for note in input_sequence])
         network_output.append(note_to_int[output_sequence])
 
+    # Normalize input
+    network_input = normalize(network_input, len(set(notes)))
+
     # Convert lists into PyTorch tensors
     network_input = torch.FloatTensor(network_input)
     network_output = torch.FloatTensor(network_output)
@@ -56,3 +61,27 @@ def create_note_to_int_dict(notes):
     note_to_int = dict((note, number) for number, note in enumerate(note_names))
 
     return note_to_int
+
+
+def normalize(sequence_list, range):
+    # Normalize the list of sequences
+    normalized_seq_list = []
+
+    for sequence in sequence_list:
+        normalized_sequence = [note / range for note in sequence]
+        normalized_seq_list.append(normalized_sequence)
+
+    return normalized_seq_list
+
+
+def split_and_load_dataset(dataset, train_val_split_percentage, train_batchsize, val_batchsize):
+    # Split dataset into train and val sets
+    num_train_data = math.floor(train_val_split_percentage * len(dataset))
+    num_val_data = len(dataset) - num_train_data
+    train_dataset, val_dataset = random_split(dataset, [num_train_data, num_val_data])
+
+    # Load data into data loaders
+    train_loader = DataLoader(dataset=train_dataset, batch_size=train_batchsize)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=val_batchsize)
+
+    return (train_loader, val_loader)
